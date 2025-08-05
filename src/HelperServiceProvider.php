fb78e3d17ee06eb4e6393ca1892b1cb7a9335e40
sharif <msharif.ahrari@gmail.com>
2025-08-05 19:49:25 +0330

    update package add many methods add tests

--- src/HelperServiceProvider.php
+++ src/HelperServiceProvider.php
@@ -8,11 +8,11 @@ class HelperServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // تنظیمات اضافی مثل روت‌ها، ویوها یا فایل‌های تنظیمات
+
     }
 
     public function register()
     {
-        // ثبت سرویس‌ها یا bindings
+
     }
 }
